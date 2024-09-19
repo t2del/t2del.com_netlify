87ae9d7232960c3f929b54851ce4f4d068c86401
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-09-19 23:23:44 +0800

    install partytown for ga4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://astrot2del.netlify.app/',
+	integrations: [
+        mdx(), 
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 });
