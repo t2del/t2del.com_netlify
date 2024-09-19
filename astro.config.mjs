75bf229ed49a00de99a519fb7688f0b03b79c73e
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-09-19 23:28:06 +0800

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown'
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astrot2del.netlify.app/',
 	integrations: [
         mdx(), 
         sitemap(),
-        partytown({
-            config: {
-              forward: ["dataLayer.push"],
-            },
-        }),
     ],
 });
