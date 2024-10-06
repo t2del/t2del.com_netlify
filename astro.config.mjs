47ef59ebfe482c72f546ad42d36fd4e71cb5772b
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-10-06 18:39:07 +0800

    remove search

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import pagefind from "astro-pagefind"
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +9,5 @@ export default defineConfig({
 	integrations: [
         mdx(), 
         sitemap(),
-        pagefind(),
     ],
 });
