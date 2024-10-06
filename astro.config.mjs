85bf89d4cb33797a868c02ee30db842cadd095ae
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-10-06 18:31:33 +0800

    add search, fix favicon routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind"
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
 	integrations: [
         mdx(), 
         sitemap(),
+        pagefind(),
     ],
 });
