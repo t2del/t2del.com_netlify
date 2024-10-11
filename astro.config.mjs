e76ede116f51fc48a36bc528462e50b96a458349
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-10-11 14:19:48 +0800

    Update astro.config.mjs
    astro site url update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astrot2del.netlify.app/',
+	site: 'https://www.t2del.com/',
 	integrations: [
         mdx(), 
         sitemap(),
