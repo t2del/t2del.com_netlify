b6d41aa1c9f0bc7d079e1776d11ca59edafea326
Dennis Macapagal <macapagal.dennis@gmail.com>
2025-01-31 09:56:22 +0800

    added ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: "https://www.t2del.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
 });
