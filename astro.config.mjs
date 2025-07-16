9769898809066c9cfa21006227a57df206b2b2bf
Dennis Macapagal <macapagal.dennis@gmail.com>
2025-07-16 23:35:34 +0800

    added pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import netlify from "@astrojs/netlify";
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: "https://www.t2del.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), tailwind(),pagefind()],
   adapter: netlify(),
 });
