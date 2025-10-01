ff0af81e28255af959a57fd3fbc8c350f8ef193e
Dennis Macapagal <macapagal.dennis@gmail.com>
2025-10-01 14:10:33 +0800

    remove astrodb in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import pagefind from "astro-pagefind";
 
-import db from "@astrojs/db";
+
 
 export default defineConfig({
   site: "https://www.t2del.com",
   build: {
     format: "file",
   },
-  integrations: [mdx(), sitemap(), tailwind(), pagefind(), db()],
+  integrations: [mdx(), sitemap(), tailwind(), pagefind()],
   adapter: netlify(),
 });
