2c07664297ebbc1c417e5e2aae2987cc1a2a81d4
Dennis Macapagal <macapagal.dennis@gmail.com>
2025-10-01 13:35:45 +0800

    db config

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,17 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
-// https://astro.build/db/config
+const Blog = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text(),
+    description: column.text(),
+    content: column.text(),
+    thumbnail: column.text({ optional: true }), // URL or path to the image
+    status: column.text({ default: 'draft' }), // e.g., 'published', 'draft', 'archived'
+    datePublished: column.date({ default: new Date() }), // Defaults to the current date
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Blog },
 });
